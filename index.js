8ad5e800c5901e04f4b5bdceffb952fdd25261a7
Darksniper514 <hudalatief2015@gmail.com>
2026-07-20 17:57:57 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,36 +4,32 @@ function createBot() {
   const bot = mineflayer.createBot({
     host: 'propvps.mcsh.io',
     port: 25565,
-    username: 'afk'
+    username: 'BotName'
   });
 
   bot.on('spawn', () => {
-    console.log('Bot successfully joined the server!');
-    startAntiAfk(bot);
+    console.log('Bot successfully joined!');
+    
+    setInterval(() => {
+      bot.setControlState('jump', true);
+      setTimeout(() => {
+        bot.setControlState('jump', false);
+      }, 250);
+    }, 15000);
+
+    setInterval(() => {
+      const yaw = Math.random() * Math.PI * 2;
+      const pitch = (Math.random() * 0.5) - 0.25;
+      bot.look(yaw, pitch, true);
+    }, 8000);
   });
 
-  function startAntiAfk(b) {
-    setInterval(() => {
-      if (!b.player) return;
-      
-      b.setControlState('jump', true);
-      setTimeout(() => {
-        b.setControlState('jump', false);
-      }, 500);
-
-      const yaw = b.entity.yaw + 1;
-      b.look(yaw, b.entity.pitch, true);
-      
-      console.log('Anti-AFK action performed.');
-    }, 60000);
-  }
-
   bot.on('kicked', (reason) => {
-    console.log('Bot was kicked due to:', reason);
+    console.log('Bot was kicked due to: ', reason);
   });
 
   bot.on('error', (err) => {
-    console.log('An error occurred:', err);
+    console.log('Error: ', err);
   });
 
   bot.on('end', () => {
