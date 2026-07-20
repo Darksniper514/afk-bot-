c592d93cdf1d299fe323b5e3ef894f59918bfa38
Darksniper514 <hudalatief2015@gmail.com>
2026-07-20 18:08:59 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,20 +5,11 @@ function createBot() {
     host: 'propvps.mcsh.io',
     port: 25565,
     username: 'BotName',
-    hideErrors: false
+    physicsEnabled: false
   });
 
   bot.once('spawn', () => {
-    console.log('Bot successfully joined!');
-    
-    setTimeout(() => {
-      setInterval(() => {
-        bot.setControlState('jump', true);
-        setTimeout(() => {
-          bot.setControlState('jump', false);
-        }, 250);
-      }, 15000);
-    }, 5000);
+    console.log('Bot successfully joined and staying still!');
   });
 
   bot.on('kicked', (reason) => {
